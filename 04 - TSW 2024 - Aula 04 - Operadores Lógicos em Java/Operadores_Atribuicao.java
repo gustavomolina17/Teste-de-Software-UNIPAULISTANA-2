005d53b93565cf795bea32f572f347e62c586624
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 09:45:09 -0300

    Create Operadores_Atribuicao.java

---
+++ 04 - TSW 2024 - Aula 04 - Operadores Lógicos em Java/Operadores_Atribuicao.java
@@ -0,0 +1,25 @@
+/* 
+ * Professor Gustavo Molina
+ * 10/09/24
+ */
+
+ public class Operadores_Atribuicao {
+
+    public static void main(String[] args) {
+
+       int numero = 18;
+       double valor = 3.14;
+       String nome = "Gustavo";
+       char letra = 'G';
+       boolean condicao = true;
+       int resultado = 2+3*4;
+
+       System.out.println("Valor Inteiro: " + numero);
+       System.out.println("Valor Double: " + valor);
+       System.out.println("O ganhador do pix misterioso é: " + nome);
+       System.out.println("Inicial do meu nome: " + letra);
+       System.out.println("Vou ganhar na Mega Sena: " + condicao);
+       System.out.println("Resultado = "+resultado);
+        
+    }   
+ }
